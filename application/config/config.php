dfd1354fe0a86c879d3cdfb31dec145b7df8af0f
Tommy MacWilliam <tmacwilliam@cs.harvard.edu>
2011-06-06 04:17:02 -0400

    updated htaccess and corresponding config

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://tommymacwilliam.com/help';
+$config['base_url']	= 'http://tommymacwilliam.com/cs50help';
 
 /*
 |--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ $config['base_url']	= 'http://tommymacwilliam.com/help';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
